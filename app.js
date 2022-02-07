dd4d2b4da25822af0aeb9bd3547c2272cb0cc635
Jessica Yuen <jess986.codes@gmail.com>
2022-02-07 11:34:36 +1100

    removed unnecessary files and added gitignore

--- app.js
+++ app.js
@@ -1,39 +1,35 @@
-const express = require('express');
-const db = require('mysql');
-const config = require('config');
+const express = require("express");
+const db = require("mysql");
+const config = require("config");
 const app = express();
 
-
-const dbConfig = config.get('Products.dbConfig');
+const dbConfig = config.get("Products.dbConfig");
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
+  const con = db.createConnection(dbConfig);
 
-    const con = db.createConnection(dbConfig);
+  con.connect((err) => {
+    if (err) throw err;
+    console.log("Connnected!");
 
-    con.connect((err) => {
-        if (err) throw err;
-        console.log("Connnected!");
+    var sql = "SELECT * FROM products where special=1";
 
-        var sql = "SELECT * FROM products where special=1";
+    con.query(sql, (err, result) => {
+      if (err) throw err;
 
-        con.query(sql, (err, result) => {
-            if (err) throw err;
+      console.log(result);
 
-            console.log(result);
+      deals = {
+        info: result,
+      };
 
-            stuff = {
-                info: result
-            };
-
-            res.render("deals", stuff);
-
-        })
-    }); 
-
+      res.render("deals", deals);
+    });
+  });
 });
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
